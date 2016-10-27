ac9694c128029dba90a38d179628d50aae3a58c7
jaeyeonkim0919 <jaeyeonkim0919@gmail.com>
2016-10-27 16:57:00 -0400

    activity_main.xml updated

--- app/src/main/java/me/erickim/android/unlockcount/MainActivity.java
+++ app/src/main/java/me/erickim/android/unlockcount/MainActivity.java
@@ -1,13 +1,22 @@
 package me.erickim.android.unlockcount;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // comment test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
+}
 }
